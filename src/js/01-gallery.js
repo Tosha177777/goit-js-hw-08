bf2e7c83526297cac0c896c99efb602fd6f71d75
Tosha177777 <slavkoanton4@gmail.com>
2023-07-29 19:57:48 +0300

    problem with import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+let largeImageUrl;
+let instance;
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', modalClick);
+gallery.addEventListener('keydown', modalCloseEsc);
+
+function modalClick(e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target === e.currentTarget) {
+    return;
+  }
+  largeImageUrl = target.parentNode.href;
+  modalOpen();
+}
+
+function modalOpen() {
+  instance = basicLightbox.create(
+    `
+		<img src="${largeImageUrl}">
+	`
+  );
+  instance.show();
+}
+
+function modalCloseEsc(e) {
+  if (e.code === 'Escape' && instance) {
+    instance.close();
+  }
+}
