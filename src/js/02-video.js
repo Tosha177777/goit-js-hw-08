32b8c362f24a82cb0a1d51f7b4f1600e9c11824c
Tosha177777 <slavkoanton4@gmail.com>
2023-07-30 23:33:14 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+  const time = e.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
